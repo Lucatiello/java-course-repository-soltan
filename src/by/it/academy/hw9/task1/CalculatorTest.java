55a984f40d99e915b5c0c95eb201b99ce9d035ef
Lucatiel <Artoriastheabusswalker@gmail.com>
2023-04-04 13:51:02 +0300

    fixed hw 9 for task 9, ready for review.

---
+++ src/by/it/academy/hw9/task1/CalculatorTest.java
@@ -0,0 +1,10 @@
+package by.it.academy.hw9.task1;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        System.out.println("Divide example: " + Calculator.divide(2, 2));//task-1
+        System.out.println("Multiply example: " + Calculator.multiply(15, 15));
+        System.out.println("Sum example: " + Calculator.sum(100, 12));
+        System.out.println("Subtract example: " + Calculator.subtraction(50, 50));
+    }
+}
