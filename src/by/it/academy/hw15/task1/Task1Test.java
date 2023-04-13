9b558f2051cd18665aabab267d403594df179aba
Lucatiel <Artoriastheabusswalker@gmail.com>
2023-04-13 17:02:34 +0300

    fix hw 15

--- src/by/it/academy/hw15/task1/Task1Test.java
+++ src/by/it/academy/hw15/task1/Task1Test.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class Task1Test {
@@ -15,10 +16,10 @@ public class Task1Test {
         System.out.println("Longest string: " + longestString);
         String shortestString = col.stream().min(Comparator.comparing(String::length)).orElse(null);
         System.out.println("Shortest string: " + shortestString);
-        List<String> wordString = col.stream().filter(str -> str.matches("\\b\\w+\\b")).toList();
+        List<String> wordString = col.stream().filter(str -> str.matches("\\b\\w+\\b")).collect(Collectors.toList());
         System.out.println("Word strings: " + wordString);
         List<String> words = col.stream().flatMap(str -> Arrays.stream(str.split("\\s+")))
-                .map(str -> str.replaceAll("\\W", "")).distinct().toList();
+                .map(str -> str.replaceAll("\\W", "")).distinct().collect(Collectors.toList());
         System.out.println("Words: " + words);
     }
 }
